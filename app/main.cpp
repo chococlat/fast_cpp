a773ed35d4db6456ea37ca11c5cf96be3e41b6af
unknown <a.ceccarini11@gmail.com>
2025-05-04 11:01:13 +0200

    save

---
+++ app/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <filesystem>
+#include "class.h"
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::wcin;
+using std::wcout;
+using std::wstring;
+namespace fs = std::filesystem;
+
+int main(){
+    // std::cout << cmd("ifconfig");
+    std::wcout << "Hello World!\n";
+    return 0;
+}
